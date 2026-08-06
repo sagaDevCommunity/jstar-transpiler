29d1a680f3f031b49ec5c737267e30e6142bdf12
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-08-05 23:06:14 -0500

    Update JScript version and input processing
    Updated version number to 0.5 and modified input handling to allow halting.

--- src/main.rb
+++ src/main.rb
@@ -2,10 +2,17 @@ require_relative 'transpiler'
 
 module JScript
   def main()
-    print "--- JScript v0.4 ---\n\nWelcome to JScript!\nEnter a line of JScript below.\n\n"
+    print "--- JScript v0.5 ---\n\nWelcome to JScript!\nEnter a line of JScript below.\n\n"
+    temp = ""
     while true
       print "> "
-      puts transpile(gets.chomp)
+      input = gets.chomp
+      if input == "#halt"
+        print temp
+        break
+      else
+        temp += transpile(input) + "\n"
+      end
     end
   end
 end
